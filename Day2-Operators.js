e4e8d0d9d1181f1df93b8ab4b78c9684667d2cd6
Yukta Pandey <yuktapandey@Yuktas-MacBook-Air.local>
2024-07-14 13:36:06 +0530

    DAY2

---
+++ Day2-Operators.js
@@ -0,0 +1,53 @@
+//ACTIVITY 1
+var sum = 10+5;
+console.log("Sum: ", sum);
+
+var sub = 10-5;
+console.log("Subtraction: ", sub);
+
+var mul = 10*5;
+console.log("Multiplication: ", mul);
+
+var div = 10/5;
+console.log("Division: ", div);
+
+var rem = 10%5;
+console.log("Remainder: ", rem);
+
+//ACTIVITY 2
+sum += 10;
+console.log("Addition: ", sum);
+
+sub -= 5;
+console.log("Subtraction: ", sub);
+
+
+//ACTIVITY 3
+console.log("is 10 greater than 5? ", 10>5);
+console.log("is 15 smaller than 20? ", 15<20);
+
+console.log("is 5 greater than equal to 5? ", 5>=5);
+console.log("is 15 smaller than equal to 20? ", 15<=20);
+
+console.log("is 15 === '15'? ", 15==='15');
+console.log("is 15 == '15'? ", 15=='15');
+
+//ACTIVITY 4
+console.log("T && T: ", true&&true);
+console.log("10&&10: ", 10&&10);
+console.log("10&&2",10&&2);
+
+console.log("T || T: ", true||true);
+console.log("10 || 10: ", 10||10);
+console.log("10 || 2",10||2);
+
+console.log("!T: ", !true);
+console.log("!10: ", !10);
+
+
+//ACTIVITY 5
+var positive = (10>=0) ? "Yes" : "No";
+console.log("Is 10 a positive number? ", positive)
+
+var negative = (-2<0) ? "Yes" : "No";
+console.log("Is -2 a negative number? ", negative)
